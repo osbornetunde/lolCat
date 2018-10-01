51d283fab706920232e18d1562aaa631c4669503
Tunde Osborne <osbornetunde@gmail.com>
2018-10-01 20:24:22 +0100

    Implentation of cat download

--- lolCat.py
+++ lolCat.py
@@ -1,14 +1,16 @@
 import os
+import cat_service
 
 
 
 def main():
-	# print the header
-	# get or create output
+	print_header()
+	folder = get_or_create_output_folder()
+	print('Found or created folder: '+ folder)
+	download_cats(folder)
+
 	# download cats
 	# display cats
-	#print('hello from main')
-	get_or_create_output_folder()
 
 
 def print_header():
@@ -18,14 +20,27 @@ def print_header():
 
 
 def get_or_create_output_folder():
-	#print(__file__)
-	base_folder = os.path.dirname(__file__)
-	print(base_folder)
+	base_folder = os.path.dirname(os.path.abspath(__file__))
 	folder = 'cat_pictures'
-	full_path = os.path.abspath(os.path.join('.', folder))
-	#print(os.path.exists(full_path))
-	#print(full_path)
+	full_path = os.path.join(base_folder, folder)
 
+	if not os.path.exists(full_path) or not os.path.isdir(full_path):
+		print('Creating new directory at {}'.format(full_path))
+		os.mkdir(full_path)
+
+	return full_path
+
+
+def download_cats(folder):
+	print('contacting server to download cats...')
+	cat_count = 8
+	for i in range(1, cat_count+1):
+		name = 'lolcat {}'.format(i)
+		print('Downloading cat ' + name)
+		cat_service.get_cat(folder, name)
+
+
+	print('done')
 
 if __name__ == '__main__':
 	main()
